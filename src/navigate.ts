3d6713782c5ad5e0372eaece2125772163e5f067
Alexander Gee <58729022+alexanderdgee@users.noreply.github.com>
2019-12-17 09:53:24 +0000

    Add basic navigation interface and tests

---
+++ src/navigate.ts
@@ -0,0 +1,9 @@
+import Coordinate = require('./coordinate');
+
+function navigate(initialX: number, initialY: number, commandSequence: string): Coordinate {
+    return {
+        x: 0,
+        y: 0
+    }
+}
+export = navigate;
---
+++ test/navigate.spec.ts
@@ -0,0 +1,72 @@
+import 'jasmine';
+import Coordinate = require('../src/coordinate');
+import navigate = require('../src/navigate');
+
+interface testCase {
+    x: number,
+    y: number,
+    commandSequence: string
+}
+
+describe('Basic navigation', function() {
+    it('stays where it started', function() {
+        let start: Coordinate = { x: 4, y: 7 };
+        let commandSequence: string = '';
+        let endPosition: Coordinate = navigate(start.x, start.y, commandSequence);
+        expect(endPosition.x).toEqual(start.x);
+        expect(endPosition.y).toEqual(start.y);
+    });
+
+    it('goes left', function() {
+        let start: Coordinate = { x: 1, y: 1 };
+        let commandSequence: string = 'L';
+        let endPosition: Coordinate = navigate(start.x, start.y, commandSequence);
+        expect(endPosition.x).toEqual(0);
+        expect(endPosition.y).toEqual(1);
+    });
+
+    it('goes right', function() {
+        let start: Coordinate = { x: 1, y: 1 };
+        let commandSequence: string = 'R';
+        let endPosition: Coordinate = navigate(start.x, start.y, commandSequence);
+        expect(endPosition.x).toEqual(2);
+        expect(endPosition.y).toEqual(1);
+    });
+
+    it('goes forward', function() {
+        let start: Coordinate = { x: 1, y: 1 };
+        let commandSequence: string = 'F';
+        let endPosition: Coordinate = navigate(start.x, start.y, commandSequence);
+        expect(endPosition.x).toEqual(1);
+        expect(endPosition.y).toEqual(2);
+    });
+
+    it('goes backward', function() {
+        let start: Coordinate = { x: 1, y: 1 };
+        let commandSequence: string = 'B';
+        let endPosition: Coordinate = navigate(start.x, start.y, commandSequence);
+        expect(endPosition.x).toEqual(1);
+        expect(endPosition.y).toEqual(0);
+    });
+});
+
+describe('Edge cases', function() {
+    // Does the robot hit a wall and fail to move
+    // or does it walk off the surface and throw an error?
+    // TODO: resolve edge cases
+    it('cannot go backward below 0', function() {
+        let start: Coordinate = { x: 0, y: 1 };
+        let commandSequence: string = 'BB';
+        let endPosition: Coordinate = navigate(start.x, start.y, commandSequence);
+        expect(endPosition.x).toEqual(0);
+        expect(endPosition.y).toEqual(0);
+    });
+
+    it('cannot go left below 0', function() {
+        let start: Coordinate = { x: 1, y: 0 };
+        let commandSequence: string = 'LL';
+        let endPosition: Coordinate = navigate(start.x, start.y, commandSequence);
+        expect(endPosition.x).toEqual(0);
+        expect(endPosition.y).toEqual(0);
+    });
+});
